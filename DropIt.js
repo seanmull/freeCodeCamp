a8197f328cbd977dd7e70b075205d39b2b6d7e1e
seanmull <kimlan065@gmail.com>
2018-01-11 18:16:03 -0800

    Create DropIt.js

---
+++ DropIt.js
@@ -0,0 +1,14 @@
+/*Drop the elements of an array (first argument), starting from the front, 
+until the predicate (second argument) returns true.
+The second argument, func, is a function you'll use to test 
+the first elements of the array to decide if you should drop it or not.
+Return the rest of the array, otherwise return an empty array.*/
+
+function dropElements(arr, func) {
+	var index = arr.findIndex(func);
+	return (index != -1) ? arr.slice(index) : [];
+}
+
+//console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;})); //[3, 4]
+//console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;})); //[1,0,1]
+console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;})); //[]
